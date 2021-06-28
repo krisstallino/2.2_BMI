0722da0d96d2796bc1f40026d5a0dda426cb9c03
krisstallino <krisstallino@gmail.com>
2021-06-28 17:09:18 +0300

    updated

--- BmiService.java
+++ BmiService.java
@@ -1,7 +1,7 @@
 public class BmiService {
     public double calculateBmi (double weight, double height) {
         double bmi;
-        bmi = (weight) / (height * 2);
+        bmi = weight / (height * 2);
         //weight must be in kg
         //height must be in meters
         return bmi;
